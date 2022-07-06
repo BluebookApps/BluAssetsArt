ea147ea5cb02c84be4c05283fbcc34fd576153a0
Davi <davi.schramm.p@hotmail.com>
2022-07-06 12:16:17 -0300

    add package

---
+++ Assets/RandomIndieDev/HypercasualMeters/Demo/Scripts/MeterUsingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MeterUsingScript : MonoBehaviour
+{
+    [Header("Meter References")] 
+    public HypercasualMeter curvedMeter;
+    public HypercasualMeter straightMeter;
+
+    [Header("UI References")] 
+    public TextMeshProUGUI valueDisplayText;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            curvedMeter.TriggerMeter();
+            straightMeter.TriggerMeter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            var curvedMeterValue = curvedMeter.GetCurrentValue();
+            var straightMeterValue = straightMeter.GetCurrentValue();
+
+            var text = "Curved Meter Value: " + curvedMeterValue + "\n" + "Straight Meter Value: " + straightMeterValue;
+
+            valueDisplayText.text = text;
+        }
+    }
+}
---
+++ Assets/RandomIndieDev/HypercasualMeters/Scripts/HypercasualMeter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HypercasualMeter : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [Tooltip("Change the speed the pointer moves at")]
+    public float pointerSpeed;
+    
+    [Tooltip("Displays")]
+    public string defaultDisplayValue;
+
+    public bool startOnPlay;
+    
+    [Header("Values")]
+    public float minPossibleValue;
+    public float maxPossibleValue;
+
+    [Header("References")]
+    public Animator _animator;
+    public TextMeshProUGUI _valueText;
+
+    [SerializeField]
+    [Tooltip("Leave this alone, using this to know where the value is currently on the slider")]
+    [Range(0, 100)]
+    private float _animationSlider;
+
+    void Start()
+    {
+        // Sets the configured speed to the animation
+        _animator.SetFloat("PointerMoveSpeed", pointerSpeed);
+        
+        // Sets user inputed default text
+        _valueText.text = defaultDisplayValue;
+        
+        if (startOnPlay)
+            _animator.SetTrigger("TriggerPointerMove");
+    }
+    private void UpdateValue(float value)
+    {
+        _valueText.text = value + "";
+    }
+
+    //                            PUBLIC FUNCTIONS
+    
+    // Gets the current value (0-100) where ever the pointer is at the time of calling this function
+    public float GetCurrentValue()
+    {
+        var sliderValue = Mathf.Round(_animationSlider);
+        var diff = maxPossibleValue - minPossibleValue;
+
+        var finalValue = (diff * sliderValue / 100) + minPossibleValue;
+        
+        finalValue = Mathf.Round(finalValue);
+        
+        UpdateValue(finalValue);
+        
+        return sliderValue;
+
+    }
+    
+    // Start / Stop the meter animation from playing
+    public void TriggerMeter()
+    {
+        _animator.SetTrigger("TriggerPointerMove");
+    }
+}
